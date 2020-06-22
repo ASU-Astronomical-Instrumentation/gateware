c68ce603ef62cc6321a4d322ad1373258ca839e2
Ryan Stephenson <ryan.c.stephenson@asu.edu>
2020-06-22 11:20:24 -0700

    syntax errors fix

--- bram/toplevel.vhd
+++ bram/toplevel.vhd
@@ -8,7 +8,7 @@ entity GSP_MMIO_v1_0_S00_AXI is
 		M : INTEGER := 32; -- Data width of a the data
 		W : INTEGER := 1; -- Number of data points present in a memory location (used to cast data to memory)
 		LD : STD_LOGIC := '1'; -- MMIO config Read/~Write from BRAM Module 
-		DATA_WIDTH	: INTEGER	:= 32;
+		DATA_WIDTH	: INTEGER	:= 32
 	);
 	port (
         reg0, reg1, reg2, reg3 : in std_logic_vector(DATA_WIDTH-1 downto 0);
@@ -22,7 +22,7 @@ entity GSP_MMIO_v1_0_S00_AXI is
         count : out std_logic_vector(N-1 downto 0);
         done : out std_logic;
         
-        UBC_clk : in std_logic;
+        UBC_clk : in std_logic
 	);
 end GSP_MMIO_v1_0_S00_AXI;
 
@@ -30,12 +30,11 @@ architecture arch_imp of GSP_MMIO_v1_0_S00_AXI is
     signal hold : std_logic;
     signal dat : std_logic_vector(N-1 downto 0);
 begin
-    gpo <= reg0(DATA_WIDTH-1 downto DATA_WIDTH-N);
-    inA <= reg1(30); -- clear count value 
-    inB <= reg1(29);
-    writeTO <= reg1(28 downto 28-W);
+    gpo <= reg0(N-1 downto 0);
+    inA <= reg1(1); -- clear count value 
+    inB <= reg1(2);
    
-    write <=  reg3(DATA_WIDTH-1 downto DATA_WIDTH-M); 
+    write <=  reg3(M-1 downto 0); 
     count <= dat;
     done <= hold; --holding max count flag (done writing values to bream)
    
@@ -43,7 +42,7 @@ begin
     generic map(N => N) --dont ask
     port map(
         clk => UBC_clk, 
-        clr => reg1(31), -- where clear count value is set
+        clr => reg1(0), -- where clear count value is set
         enb => hold, --PLACE HOLDER
         Q => dat
    );
