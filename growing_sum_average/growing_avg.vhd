6c57bff9602fbe5f3babdb2079592bf8c3960d6a
Ryan Stephenson <ryanstephenson2013@gmail.com>
2020-10-07 12:50:18 -0700

    new control logic

--- growing_sum_average/growing_avg.vhd
+++ growing_sum_average/growing_avg.vhd
@@ -9,8 +9,10 @@ entity growing_avg is
     );
     port(
         clk : in std_logic;
+      valid : in std_logic;
           x : in std_logic_vector(N-1 downto 0);
-  N_AVGS_in : in std_logic_vector(3-1 downto 0);     
+  N_AVGS_in : in std_logic_vector(3-1 downto 0);
+    new_dat : out std_logic;     
           y : out std_logic_vector(N-1 downto 0)
     );
 end growing_avg;
@@ -23,26 +25,39 @@ architecture behv of growing_avg is
     signal result : std_logic_vector(N-1 downto 0);
 begin 
     
-    acc : process(clk, N_AVGS_in) --accumulation
+    acc : process(clk, rdy, N_AVGS_in) --accumulation
     begin 
-        if (clk'event and clk='1') then   
-                if (adds = 0) then
-                    sum <=  first_val + unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
-                    adds <= (SUM_WIDTH-1 downto 1 => '0') & '1'; -- (0 => '1', others =>'0')
-                elsif (adds < 2**(to_integer(unsigned(N_AVGS_in))) -1) then -- continue accumulation until max average # is reached
-                    sum <= sum + unsigned((SUM_WIDTH-1 downto x'length => '0') & x); -- sum = sum + x
-                    adds <= adds + 1;   
-                else    
-                    adds <= (others => '0');
-                    first_val <= unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
-                    sum_out <= sum;
+        if (clk'event and clk='1') then  
+                new_data <='0'; 
+                if (valid='1') then
+                    if (adds = 0) then
+                        sum <=  first_val + unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
+                        adds <= (SUM_WIDTH-1 downto 1 => '0') & '1'; -- (0 => '1', others =>'0')
+                    elsif (adds < 2**(to_integer(unsigned(N_AVGS_in))) -1) then -- continue accumulation until max average # is reached
+                        sum <= sum + unsigned((SUM_WIDTH-1 downto x'length => '0') & x); -- sum = sum + x
+                        adds <= adds + 1;   
+                    else    
+                        adds <= (others => '0');
+                        first_val <= unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
+                        sum_out <= sum;
+                        new_dat <= '1';
+                    end if;
+                else 
+                        sum_out <= (others => '0');
+                        sum <= (others => '0');
+                        adds <= (others => '0');
+                        first_val <= (others => '0');
                 end if;
         end if;   
     end process;
 
     -- synthesized with lut, not a end path for next FF. May rise issues in critical timing path.
-    result <= std_logic_vector(sum_out(N+to_integer(unsigned(N_AVGS_in))-1 downto to_integer(unsigned(N_AVGS_in)))); -- divide and slice
-    y <= result;
+    div : process(clk, sum_out)  
+        if (clk'event and clk='1') then
+            result <= std_logic_vector(sum_out(N+to_integer(unsigned(N_AVGS_in))-1 downto to_integer(unsigned(N_AVGS_in)))); -- divide and slice
+            y <= result;          
+        end if;
+    end process;
 
 end behv;
 
