14a2dbe29db21e66af0e66ab746fbf9a4756aba9
RCS <41275564+rcstephenson@users.noreply.github.com>
2020-10-07 10:09:32 -0700

    added tyler's notes
    added comments, notes and suggestions from meeting with tyler

--- growing_sum_average/growing_avg.vhd
+++ growing_sum_average/growing_avg.vhd
@@ -17,7 +17,7 @@ end growing_avg;
 
 architecture behv of growing_avg is 
     signal sum  : unsigned(SUM_WIDTH-1 downto 0); 
-    signal sum_out  : unsigned(SUM_WIDTH-1 downto 0);
+    signal sum_out  : unsigned(SUM_WIDTH-1 downto 0); -- init signal for sim
     signal first_val  : unsigned(SUM_WIDTH-1 downto 0);
     signal adds : unsigned(SUM_WIDTH-1 downto 0); -- number of additons performed
     signal result : std_logic_vector(N-1 downto 0);
@@ -28,20 +28,37 @@ begin
         if (clk'event and clk='1') then   
                 if (adds = 0) then
                     sum <=  first_val + unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
-                    adds <= (SUM_WIDTH-1 downto 1 => '0') & '1'; 
+                    adds <= (SUM_WIDTH-1 downto 1 => '0') & '1'; -- (0 => '1', others =>'0')
                 elsif (adds < 2**(to_integer(unsigned(N_AVGS_in))) -1) then -- continue accumulation until max average # is reached
                     sum <= sum + unsigned((SUM_WIDTH-1 downto x'length => '0') & x); -- sum = sum + x
                     adds <= adds + 1;   
-                else 
+                else    
                     adds <= (others => '0');
                     first_val <= unsigned((SUM_WIDTH-1 downto x'length => '0') & x);
                     sum_out <= sum;
                 end if;
-        end if;
-      
+        end if;   
     end process;
- 
+    -- synthesized with lut, not a end path for next FF. May rise issues in critical timing path.
     result <= std_logic_vector(sum_out(N+to_integer(unsigned(N_AVGS_in))-1 downto to_integer(unsigned(N_AVGS_in)))); -- divide and slice
     y <= result;
 
 end behv;
+
+-- Notes, Overflow
+-- sum <= ('0' & operand1) + ('0' & operand2);
+-- sum <= (operand1(N-1) & operand1) + (operand2(N-1) & operand2);
+
+-- sum <= (operand1(N-1) & '0' & operand1) + (operand2(N-1) & '0' & operand2);
+
+-- overflow_flag <= sum(n-2)
+-- operand = 7 bits
+-- sum = 9 bits
+
+-- VHDLwhiz random verification
+
+-- https://www.adiuvoengineering.com/blog/categories/microzed-chronicles (past 4 articles)
+-- https://vhdlwhiz.com/constrained-random-verification/
+
+-- https://vhdlwhiz.com/ring-buffer-fifo/ (might not be ideal for higher clock rates)
+
