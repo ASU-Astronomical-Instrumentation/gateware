ae8842187d07b9878541eb81766da8cebf7ef97c
RCS <41275564+rcstephenson@users.noreply.github.com>
2020-10-09 14:00:30 -0700

    simplified structure

--- ethernet/UDP_packetizer/pfb_data_handler.vhd
+++ ethernet/UDP_packetizer/pfb_data_handler.vhd
@@ -3,27 +3,32 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity pfbDH is 
-      generic( 	N : integer := 82;
-	            ROMLEN : integer := 3*1024 ); 
+      generic( 	
+                N_channels : integer := 4;
+                N : integer := 17;    
+	            ROMLEN : integer := 1024*N_channels*N/8 ); 
 	port( 
 		wclk    : in std_logic;
 		rclk    : in std_logic;
-        wdata   : in std_logic_vector(N-1 downto 0);
-        rdata   : out std_logic_vector(N-1 downto 0); 
-        waddr   : in std_logic_vector(9 downto 0);
-        raddr   : in std_logic_vector(9 downto 0);
-        rready  : in std_logic;
-        bramrdy : out std_logic;
-        clr     : in std_logic;
-        done    : out std_logic
+        wdata   : in array(0 to N_channels*N/8 -1) of std_logic_vector(7 downto 0);
+        rdata   : out std_logic_vector(7 downto 0); 
+        --waddr   : in std_logic_vector(9 downto 0);
+        --raddr   : in std_logic_vector(9 downto 0);
+        emptied : out std_logic;
+        filled  : out std_logic;
+        reader_valid   : in std_logic;
+        writer_valid  : in std_logic;
+        --clear   : in std_logic;
+        --done    : out std_logic
 	);
 end pfbDH;
 
 
 architecture behv of pfbDH is 
-    type ROM is array(0 to ROMLEN) of std_logic_vector(N-1 downto 0); 
+    type ROM is array(0 to ROMLEN) of std_logic_vector(7 downto 0); 
     signal TABLE : ROM;--  = (others => (others => 0)); 
-    signal counter : unsigned(9 downto 0);
+    signal wcounter : unsigned(9 downto 0);
+    signal rcounter : unsigned(9 downto 0);
     signal rw  : std_logic; -- rw = reading from bram, ~rw = writing to bram
 begin 
 
@@ -32,12 +37,14 @@ begin
     begin 
         if (wclk'event and wclk='1') then 
             if (rw='0') then
-                if (to_integer(counter) > ROMLEN-1 ) then
-                    counter <= (others => '0');
+                if (to_integer(wcounter) = ROMLEN-1 ) then
+                    wcounter <= (others => '0');
                     rw <='1';
+                elsif (writer_valid = '1')
+                    wcounter <= wcounter + 1;
+                    TABLE(to_integer(wcounter)) <= wdata;
                 else 
-                    counter <= counter + 1;
-                    TABLE(to_integer(counter)) <= wdata;
+                    -- waiting to read
                 end if;
             end if;
         end if;
@@ -45,24 +52,19 @@ begin
 
 	
     readfrom: process(rclk,rw)
-        variable read : integer; 
     begin
-        if (rclk'event and rclk='1') then
-            done <= '0'; 
-            if (rw= '1') then
-                bramrdy <= '1';
-                if (rready='1') then 
-                    rdata <= TABLE(to_integer(unsigned(raddr)));
-                    -- TODO: add idle
-                else   
-                    done <='1';
+        if (rclk'event and rclk='1') then 
+            if (rw = '1') then
+                if (reader_valid='1') then 
+                    rdata <= TABLE(to_integer(unsigned(rcounter)));
+                    rcounter <= '1';
+                elsif ( to_integer(rcounter) = ROMLEN-1)   
                     rw    <= '0';
                     TABLE <= (others => (others => '0'));  
-                    rdata <= X"deadbeef"; 
-                end if;
-            else 
-                bramrdy <='0';
-                rdata <= (others => '0');                
+                    rdata <= (others => '0'); 
+                else 
+                    -- waiting to read
+                end if;        
             end if;
         end if;
     end process;
